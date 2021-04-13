73b335829b01451b115a2484f9dea592506d7836
Simon51584 <simon51584@gmail.com>
2021-04-12 19:32:16 -0500

    added assessment file

---
+++ assement.js
@@ -0,0 +1,40 @@
+// Write a function that given a list of countries, returns the country(s) that occur the least times
+// i.e - ["Argentina", "Cuba", "Chile", "Argentina"] -> ["Cuba", "Chile"] -----------***
+
+// Deliverables. 
+// Write a function given an array []
+// compare the objects within the array
+// output objects that do not repeat
+
+arr = ["Argentina", "Cuba", "Chile", "Argentina"]
+    
+
+countries (arr) => {
+   // if statement goes here to compare objects within array
+   // loop through and compare objects
+   // return objects in array that do not repeat
+    
+    
+}
+return arr  
+
+
+Write a program to find longest Palindromic Substring within a string.Also find the start index and the end index of that Palindromic Substring within the string. Print the palindromic substring, the start index, the end index and also the original string removing the palindromic substring.
+i.e thequickbrownfoxxofnworbquickthe ->  brownfoxxofnworb
+The start index : 8, end index :23
+The string without palindromic substring :thequickquickthe
+
+// Deliverables
+// find the palindroimic substring
+// establish the starting index : 8
+// end index : 23
+// output should be :thequickquickthe
+
+
+Write a program to find the index of a value in a sorted array. If the value does not find return the index where it would be if it were inserted in order.
+Example: 
+[1, 2, 4, 5, 6] 5(target)
+ -> 3(index)
+[1, 2, 4, 5, 6] 0(target)
+ -> 0(index)
+[1, 2, 4, 5, 6] 7(target)
